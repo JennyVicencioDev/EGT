7c977b1225eac7061d7e2694dcbc56018dc96324
JennyVicencio <fervcnc@gmail.com>
2023-07-14 16:59:12 -0400

    flujo pasajes asientos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(function () {
    'use strict';
 
+   // selección de asiento
+   $('.bus-seats-lines .seat').on('click', function () {
+      let isSelectableSeat = $(this).hasClass('seat--locked') || $(this).hasClass('seat--cashier') || $(this).hasClass('seat--na');
+      if( !isSelectableSeat ) {
+         $(this).toggleClass('selected');
+      }
+   });;
   
 });
